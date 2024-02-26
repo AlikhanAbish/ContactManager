c2c67ec154c59cd370ec21ab647097ab94d504bc
AlikhanAbish <160414731+AlikhanAbish@users.noreply.github.com>
2024-02-26 18:34:49 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from ContactManager import ContactManager
 
 
-if __name__ == '__main__':
-
+if __name__ == "__main__":
     contact_manager = ContactManager("contacts.json")
 
     while True:
         print("\nMenu:")
         print("1. Add Contact")
         print("2. Delete Contact")
-        print("3. Display Contacts")
-        print("4. Exit")
+        print("3. Search Contact")
+        print("4. Edit Contact")
+        print("5. Display Contacts")
+        print("6. Exit")
 
         choice = input("Enter your choice: ")
 
@@ -22,16 +23,34 @@ if __name__ == '__main__':
             contact_manager.add_contact(name, surname, phone, email)
             print("Contact added successfully.")
         elif choice == '2':
-            print("Please enter name and surname to delete:")
-            name = input("Name: ")
-            surname = input("Surname: ")
+            name = input("Enter name to delete: ")
+            surname = input("Enter surname to delete: ")
             if contact_manager.delete_contact(name, surname):
-                print("Contact:", name, surname, "deleted successfully.")
+                print("Contact deleted successfully.")
             else:
                 print("Contact not found.")
         elif choice == '3':
-            contact_manager.display_contacts()
+            query = input("Enter name or surname: ")
+            found_contacts = contact_manager.search_contact(query)
+            if found_contacts:
+                print("Found Contacts:")
+                for contact in found_contacts:
+                    print(f"Name: {contact['name']}, Phone: {contact['phone']}, Email: {contact['email']}")
+            else:
+                print("No contacts found.")
+
         elif choice == '4':
+            name = input("Enter name of contact to edit: ")
+            surname = input("Enter surname of contact to edit: ")
+            new_phone = input("Enter new phone number: ")
+            new_email = input("Enter new email address: ")
+            if contact_manager.edit_contact(name, surname, new_phone, new_email):
+                print("Contact edited successfully.")
+            else:
+                print("Contact not found.")
+        elif choice == '5':
+            contact_manager.display_contacts()
+        elif choice == '6':
             print("Exiting...")
             break
         else:
